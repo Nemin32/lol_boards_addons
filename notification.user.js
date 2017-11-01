f92b39fe43bce4c73780101587a1279fabaecef1
Lorant P <pergerlori@gmail.com>
2017-11-01 20:14:21 +0100

    Create notification.user.js

---
+++ notification.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         LoL Boards Értesítő
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Értesít, ha üzeneted érkezett.
+// @author       Nemin
+// @match        https://*.leagueoflegends.com/hu/*
+// @exclude      https://boards.eune.leagueoflegends.com/hu/myupdates
+// @exclude      https://boards.eune.leagueoflegends.com/hu/myupdates?show=unread
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    /*fetch("https://boards.eune.leagueoflegends.com/hu/myupdates", {credentials: "include"}).then(a => a.text()).then(b => {
+        if (b.search('<div class="empty-state">') == -1) {
+            alert("Üzeneted érkezett!");
+        }
+    });*/
+    fetch("https://boards.eune.leagueoflegends.com/en/myupdates.json?show=unread", {credentials: "include"}).then(a => a.json()).then(b => {
+    if (b.resultsCount > 0) alert(b.resultsCount + " új üzeneted érkezett!");
+    });
+})();
