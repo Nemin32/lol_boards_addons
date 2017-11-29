96d999fd61f3e93c683b54d54af21ff574ff278f
Lorant P <pergerlori@gmail.com>
2017-11-29 20:28:33 +0100

    Update topicpoints.user.js

--- topicpoints.user.js
+++ topicpoints.user.js
@@ -1,38 +1,55 @@
 // ==UserScript==
 // @name         LoL Boards Points
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Profiloldali pontok
 // @author       Nemin
 // @match        https://boards.eune.leagueoflegends.com/*/player/*/*
 // @grant        none
 // ==/UserScript==
 
-/* jshint ignore:start */
 let returnPoints = (discussion_id, discussion_app_id) => {
     return new Promise((resolve, reject) => {
-        const request = fetch("https://boards.eune.leagueoflegends.com/api/"+discussion_app_id+"/discussions/"+discussion_id)
+        const request = fetch("https://boards.eune.leagueoflegends.com/api/"+discussion_app_id+"/discussions/"+discussion_id);
         request.then(resp => resp.json()).then(jsonResp => {
-            resolve(jsonResp.discussion.upVotes - jsonResp.discussion.downVotes)
-        })
-    })
-}
+            resolve(jsonResp.discussion.upVotes - jsonResp.discussion.downVotes);
+        });
+    });
+};
 
-(function() {
-    let rows = document.querySelectorAll(".discussion-list-item")
-    for (let i in rows) {
+let didAlready = 0;
+
+function updatePoints() {
+    let rows = document.querySelectorAll(".discussion-list-item");
+    for (let i = didAlready; i < rows.length; i++) {
         try {
-        const disc_id = rows[i].getAttribute("data-discussion-id")
-        const app_id = rows[i].getAttribute("data-application-id")
-        const originalText = rows[i].querySelector(".title-span").textContent
+            const disc_id = rows[i].getAttribute("data-discussion-id");
+            const app_id = rows[i].getAttribute("data-application-id");
+            const originalText = rows[i].querySelector(".title-span").textContent;
 
 
-        returnPoints(disc_id, app_id).then(resp => {
-            rows[i].querySelector(".title-span").textContent = "[" + resp + "] " + originalText
-        })
+            returnPoints(disc_id, app_id).then(resp => {
+                rows[i].querySelector(".title-span").textContent = "[" + resp + "] " + originalText;
+            });
         } catch(e) {
             console.log("Points could not be read.");
         }
     }
+    didAlready = rows.length;
+}
+
+
+function hook() {
+    try {
+        document.querySelector(".show-more").addEventListener("click", (function() {setTimeout(updatePoints, 4000);}));
+        console.log("[Topic Points] Button found");
+    } catch(e) {
+        console.log("[Topic Points] Button not found.");
+    }
+}
+
+
+(function() {
+    updatePoints();
+    hook();
 })();
-/* jshint ignore:end */
