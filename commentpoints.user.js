13dca4e5d69992964dda82dd6ea3abfe6ee3582e
L. P <2953293+Nemin32@users.noreply.github.com>
2018-04-29 17:09:22 +0200

    Update commentpoints.user.js

--- commentpoints.user.js
+++ commentpoints.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         LoL Boards Komment Pontok
+// @name         LoL Boards Comment Points
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Kijelzi a pontokat a kommentek mellett.
+// @version      0.3
+// @description  Shows how many points a comment has.
 // @author       Nemin
 // @match        *://boards.eune.leagueoflegends.com/hu/player/*/*
 // @grant        none
@@ -10,7 +10,7 @@
 
 function crawler(data, target) {
     for (let i in data) {
-        if (data[i].id == target) return (data[i].upVotes - data[i].downVotes);
+        if (data[i].id == target) return [data[i].upVotes, data[i].downVotes];
 
         let res =crawler(data[i].replies.comments, target);
 
@@ -25,7 +25,8 @@ function handle(comment) {
 
     req.then(resp => resp.json()).then(
         jsonresp => {
-            comment.querySelector(".body").innerHTML = ("<b>[" + crawler(jsonresp.comments, comment.getAttribute("data-comment-id")) + "]</b>") + comment.querySelector(".body").innerHTML;
+            let response = crawler(jsonresp.comments, comment.getAttribute("data-comment-id"));
+            comment.querySelector(".body").innerHTML = ((response == -1) ? "[ERROR]" : ("<b>[" + response[0] + " / " + ((response[1] != 0) ? "-" : "") + response[1] + "]   (" + (response[0]-response[1]) + ")</b>"))+ comment.querySelector(".body").innerHTML;
         }
     );
 }
