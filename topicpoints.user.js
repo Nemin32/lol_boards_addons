b60c54496316315666a05652ef2b3f3af28e5929
L. P <2953293+Nemin32@users.noreply.github.com>
2018-04-29 17:11:00 +0200

    Update topicpoints.user.js

--- topicpoints.user.js
+++ topicpoints.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         LoL Boards Points
+// @name         LoL Boards Post Points
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Profiloldali pontok
+// @version      0.3
+// @description  Shows you how many points your topics got.
 // @author       Nemin
 // @match        https://boards.eune.leagueoflegends.com/*/player/*/*
 // @grant        none
@@ -12,7 +12,7 @@ let returnPoints = (discussion_id, discussion_app_id) => {
     return new Promise((resolve, reject) => {
         const request = fetch("https://boards.eune.leagueoflegends.com/api/"+discussion_app_id+"/discussions/"+discussion_id);
         request.then(resp => resp.json()).then(jsonResp => {
-            resolve(jsonResp.discussion.upVotes - jsonResp.discussion.downVotes);
+            resolve([jsonResp.discussion.upVotes, jsonResp.discussion.downVotes]);
         });
     });
 };
@@ -29,7 +29,7 @@ function updatePoints() {
 
 
             returnPoints(disc_id, app_id).then(resp => {
-                rows[i].querySelector(".title-span").textContent = "[" + resp + "] " + originalText;
+                rows[i].querySelector(".title-span").textContent = "[" + resp[0] + " / " + ((resp[1] == 0) ? "" : "-") + resp[1] + "] (" + (resp[0]-resp[1]) + ") " + originalText;
             });
         } catch(e) {
             console.log("Points could not be read.");
