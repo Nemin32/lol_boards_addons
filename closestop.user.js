e8ccae8c242e1e8907f77c61ccd27eea657692eb
L. P <2953293+Nemin32@users.noreply.github.com>
2018-05-31 21:17:06 +0200

    Update closestop.user.js

--- closestop.user.js
+++ closestop.user.js
@@ -1,29 +1,41 @@
 // ==UserScript==
 // @name         LoL Boards Accidental Close Stopper
 // @namespace    http://nemin32.github.io/
-// @version      0.2
+// @version      0.3
 // @description  Stops you from accidentaly closing the page while writing a comment
 // @author       Nemin
+// @match        https://boards.eune.leagueoflegends.com/*/submit
 // @match        https://boards.eune.leagueoflegends.com/*/c/*
+// @match        https://boards.euw.leagueoflegends.com/*/submit
 // @match        https://boards.euw.leagueoflegends.com/*/c/*
 // @grant        none
 // ==/UserScript==
 
 const exit = (ev) => {ev.returnValue = "Do you really want to close this page?"; return "Do you really want to close this page?"}
 
+const body = document.querySelector("#body")
+
 function handle() {
-    const texts = [...document.querySelectorAll(".expanding-clone")];
+    if (body != null) {
+        const texts = [...document.querySelectorAll(".expanding-clone")];
 
-    let found = false;
-    texts.some(outer => {
-        const text = outer.querySelector("span").innerHTML;
-        if (text != "") {found = true; return true;}
-    })
+        let found = false;
+        texts.some(outer => {
+            const text = outer.querySelector("span").innerHTML;
+            if (text != "") {found = true; return true;}
+        })
 
-    if (found) {
-        window.addEventListener("beforeunload", exit);
+        if (found) {
+            window.addEventListener("beforeunload", exit);
+        } else {
+            window.removeEventListener("beforeunload", exit);
+        }
     } else {
-        window.removeEventListener("beforeunload", exit);
+        if (body.value.length > 30) {
+            window.addEventListener("beforeunload", exit);
+        } else {
+            window.removeEventListener("beforeunload", exit);
+        }
     }
 }
 
